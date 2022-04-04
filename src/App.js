b34352a3f85231b60a881c29031a54ebc6a3c4af
Yuuki915 <aso091335@gmail.com>
2022-04-04 16:37:57 -0700

    modi files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
+import Product from "./components/product-pages/Product";
 
 function App() {
   return (
     <div className="App">
+      <Product />
       <Header />
 
       <Main />
