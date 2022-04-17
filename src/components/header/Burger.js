0ae99193b2770e54745e9c4f04a104ed1019fbb1
Yuuki915 <aso091335@gmail.com>
2022-04-16 23:40:18 -0700

    prog ham-menu

--- src/components/header/Burger.js
+++ src/components/header/Burger.js
@@ -2,8 +2,30 @@
 
 import React from "react";
 import { CgMenuRightAlt } from "react-icons/cg";
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
+import Navbar from "./Navbar";
 
 export default function Burger() {
+  const [open, setOpen] = useState(false);
+
+  const openHamIcon = (
+    <CgMenuRightAlt
+      className="hamburger"
+      size="30px"
+      color="#333"
+      onClick={() => setOpen(!open)}
+    />
+  );
+  const closeHamIcon = (
+    <MdClose
+      className="hamburger"
+      size="30px"
+      color="#333"
+      onClick={() => setOpen(!open)}
+    />
+  );
+
   return (
     <div id="outer-container">
       {/* <Menu
@@ -13,12 +35,10 @@ export default function Burger() {
         width={280}
         disableCloseOnEsc
       /> */}
-      <CgMenuRightAlt
-        className="hamburger"
-        size="30px"
-        color="#333"
-        onClick={() => console.log("hi")}
-      />
+
+      {open ? closeHamIcon : openHamIcon}
+      {open && <Navbar />}
+
       <main id="page-wrap"></main>
     </div>
   );
