c0f047dace7bff8ee54a5a7a9fdfcd2952d78181
Yuuki915 <aso091335@gmail.com>
2022-04-18 13:14:38 -0700

    modi App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import "./App.css";
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
-function App() {
-  return <div className="App"></div>;
-=======
 import Product from "./components/product-pages/Product";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
@@ -20,25 +14,6 @@ function App() {
       </Router>
     </div>
   );
->>>>>>> Stashed changes
-=======
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Main from "./components/main/Main";
-import Product from "./components/product-pages/Product";
-
-function App() {
-  return (
-    <div className="App">
-      <Product />
-      <Header />
-
-      <Main />
-
-      <Footer />
-    </div>
-  );
->>>>>>> eba8bf3733c6045be87a0a4d05d0270bb7242a24
 }
 
 export default App;
